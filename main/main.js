87987df6b4f5588929b2baf1fa2395b68d03d37a
IYAN OLIVER ALVAREZ (PLAN-IRIS-ISD-OOCLL/MNL) <ALVARIY@OOCL.COM>
2019-10-07 13:21:44 +0800

    Add products array

--- main/main.js
+++ main/main.js
@@ -2,6 +2,7 @@ module.exports = function printReceipt(inputs) {
     console.log("Debug Info");
 
     var sum = inputs.map(input => input.Price).reduce((prev, next) => prev + next, 0);
+    var products = new Array(3);
     var cokeCount = 0;
     var cokeSubTotal = 0.00;
     var cokeUnitPrice = 0.00;
@@ -21,26 +22,29 @@ module.exports = function printReceipt(inputs) {
             cokeCount = cokeCount + 1;
             cokeSubTotal = cokeSubTotal + inputs[i].Price;
             cokeUnitPrice = inputs[i].Price;
+            products[0] = inputs[i].Name;
         }
         else if (inputs[i].Barcode == 'ITEM000001')
         {
             spriteCount = spriteCount + 1;
             spriteSubTotal = spriteSubTotal + inputs[i].Price;
             spriteUnitPrice = inputs[i].Price;
+            products[1] = inputs[i].Name;
         }
         else if (inputs[i].Barcode == 'ITEM000004')
         {
             batteryCount = batteryCount + 1;
             batterySubTotal = batterySubTotal + inputs[i].Price;
             batteryUnitPrice = inputs[i].Price;
+            products[2] = inputs[i].Name;
         }
     }
 
 
     return   '***<store earning no money>Receipt ***\n' +
-                         'Name: Coca-Cola, Quantity: '+cokeCount+' bottles, Unit price: '+parseFloat(String(cokeUnitPrice)).toFixed(2)+' (yuan), Subtotal: '+parseFloat(String(cokeSubTotal)).toFixed(2)+' (yuan)\n' +
-                         'Name: Sprite, Quantity: '+spriteCount+' bottles, Unit price: '+parseFloat(String(spriteUnitPrice)).toFixed(2)+' (yuan), Subtotal: '+parseFloat(String(spriteSubTotal)).toFixed(2)+' (yuan)\n' +
-                         'Name: Battery, Quantity: '+batteryCount+', Unit price: '+parseFloat(String(batteryUnitPrice)).toFixed(2)+' (yuan), Subtotal: '+parseFloat(String(batterySubTotal)).toFixed(2)+' (yuan)\n' +
+                         'Name: '+products[0]+', Quantity: '+cokeCount+' bottles, Unit price: '+parseFloat(String(cokeUnitPrice)).toFixed(2)+' (yuan), Subtotal: '+parseFloat(String(cokeSubTotal)).toFixed(2)+' (yuan)\n' +
+                         'Name: '+products[1]+', Quantity: '+spriteCount+' bottles, Unit price: '+parseFloat(String(spriteUnitPrice)).toFixed(2)+' (yuan), Subtotal: '+parseFloat(String(spriteSubTotal)).toFixed(2)+' (yuan)\n' +
+                         'Name: '+products[2]+', Quantity: '+batteryCount+', Unit price: '+parseFloat(String(batteryUnitPrice)).toFixed(2)+' (yuan), Subtotal: '+parseFloat(String(batterySubTotal)).toFixed(2)+' (yuan)\n' +
                          '----------------------\n' +
                          'Total: 23.00 (yuan)\n' +
                          '**********************\n';
